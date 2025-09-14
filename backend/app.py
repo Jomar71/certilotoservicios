f5f181693ae5f630cf8e2a2f1ffe0257b079e54d
George <jorgenriquemartinezc@gmail.com>
2025-09-14 16:50:28 -0500

    Deploy ready with Render DB

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,14 @@
-from flask import Flask, jsonify, request, send_file
+from flask import Flask
 from flask_cors import CORS
 from routes.admin import admin_bp
 from routes.certificate import certificate_bp
-from db import db
-from config import Config
+from db import db, init_db
 
 app = Flask(__name__)
 CORS(app)
 
-# Configuración de la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = Config.DATABASE_URL
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 # Inicializar la base de datos
-db.init_app(app)
+init_db(app)
 
 # Registrar blueprints
 app.register_blueprint(admin_bp, url_prefix='/admin')
@@ -21,7 +16,7 @@ app.register_blueprint(certificate_bp, url_prefix='/certificate')
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Bienvenido a Certilotoservicios API"}), 200
+    return {"message": "Bienvenido a Certilotoservicios API"}, 200
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/db.py
+++ backend/db.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 db = SQLAlchemy()
+
+def init_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
